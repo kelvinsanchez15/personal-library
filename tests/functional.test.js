5a6be9ad6d612e12fa66e956adc55b0cf629fea6
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-08-21 17:21:44 -0400

    add all test cases

--- tests/functional.test.js
+++ tests/functional.test.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../app');
 const Book = require('../models/book');
 
-beforeEach(async () => {
+const mockBook = { title: 'The Way of Kings' };
+const mockComment = { comment: 'The best book ever!' };
+
+beforeAll(async () => {
   await Book.deleteMany();
 });
 
@@ -12,11 +16,62 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe('Functional Test', () => {
-  test('testing tests', async () => {
-    const response = await request(app)
-      .post('/api/books')
-      .send({ title: 'The Way of Kings' });
-    expect(response.status).toBe(201);
+describe('Functional Tests', () => {
+  describe('POST /api/books with title => create book object/expect book object', () => {
+    test('Test POST /api/books with title', async () => {
+      const response = await request(app).post('/api/books').send(mockBook);
+      expect(response.status).toBe(201);
+      expect(response.body).toMatchObject(mockBook);
+    });
+
+    test('Test POST /api/books with no title given', async () => {
+      const response = await request(app)
+        .post('/api/books')
+        .send({ title: '' });
+      expect(response.status).toBe(400);
+      expect(response.text).toBe('missing title');
+    });
+  });
+
+  describe('GET /api/books => array of books', () => {
+    test('Test GET /api/books', async () => {
+      const response = await request(app).get('/api/books');
+      expect(response.status).toBe(200);
+      expect(response.body[0]).toHaveProperty('_id');
+      expect(response.body[0]).toHaveProperty('title');
+      expect(response.body[0]).toHaveProperty('commentcount');
+    });
+  });
+
+  describe('GET /api/books/[id] => book object with [id]', () => {
+    test('Test GET /api/books/[id] with id not in db', async () => {
+      const response = await request(app).get(
+        '/api/books/5f4025b7007fa826bc13a525'
+      );
+      expect(response.status).toBe(404);
+      expect(response.text).toBe('no book exists');
+    });
+
+    test('Test GET /api/books/[id] with valid id in db', async () => {
+      const book = await Book.findOne();
+
+      const response = await request(app).get(`/api/books/${book._id}`);
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject(mockBook);
+    });
+  });
+
+  describe('POST /api/books/[id] => add comment/expect book object with id', () => {
+    test('Test POST /api/books/[id] with comment', async () => {
+      const book = await Book.findOne();
+
+      const response = await request(app)
+        .post(`/api/books/${book._id}`)
+        .send(mockComment);
+      expect(response.status).toBe(201);
+      expect(response.body).toHaveProperty('_id');
+      expect(response.body).toHaveProperty('title');
+      expect(response.body).toHaveProperty('comments', [mockComment.comment]);
+    });
   });
 });

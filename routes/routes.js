c6ed4b45d1494a5342bfe9745fe99ad61032f3c7
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-08-21 12:25:07 -0400

    create all api routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,176 +1,112 @@
+/* eslint-disable no-underscore-dangle */
 const express = require('express');
-// const Book = require('../models/book');
-// const Comment = require('../models/comment');
+const Book = require('../models/book');
 
 const router = express.Router({ mergeParams: true });
 
-// ======================
-// CREATE NEW ISSUE
-// ======================
-// router.get('/new', async (req, res) => {
-//   const projectName = req.params.project;
+router
+  .route('/')
+  // ======================
+  // CREATE NEW BOOK
+  // ======================
+  .post(async (req, res) => {
+    try {
+      const book = await Book.create(req.body);
+      res.status(201).json({ title: book.title, _id: book._id });
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  })
 
-//   res.render('new', { projectName });
-// });
+  // ======================
+  // READ ALL BOOKS
+  // ======================
+  .get(async (req, res) => {
+    try {
+      const books = await Book.find({});
+      const orderedJSON = books.map((e) => ({
+        _id: e._id,
+        title: e.title,
+        commentcount: e.comments.length,
+      }));
 
-// router
-//   .route('/')
-//   .post(async (req, res) => {
-//     const projectName = req.params.project;
-//     const issue = new Issue(req.body.issue);
+      res.status(200).send(orderedJSON);
+    } catch (err) {
+      res.status(404).send(err);
+    }
+  })
+  // ======================
+  // DESTROY ALL BOOKS
+  // ======================
+  .delete(async (req, res) => {
+    try {
+      await Book.deleteMany({});
 
-//     try {
-//       await issue.save();
-//     } catch (err) {
-//       return res.status(400).send(err);
-//     }
+      res.status(200).send('complete delete successful');
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
 
-//     try {
-//       // Find project by name and update pushing new issue
-//       // If no project is found create one using upsert
-//       await Project.findOneAndUpdate(
-//         { name: projectName },
-//         { $push: { issues: issue } },
-//         { new: true, upsert: true, useFindAndModify: false }
-//       );
-//       return res.status(201).redirect(`/${projectName}/issues`);
-//     } catch (err) {
-//       return res.status(400).send(err);
-//     }
-//   })
+router
+  .route('/:_id')
+  // ======================
+  // CREATE NEW COMMENT
+  // ======================
+  .post(async (req, res) => {
+    const bookId = req.params._id;
+    const { comment } = req.body;
 
-// ======================
-// READ ISSUES
-// ======================
-// .get(async (req, res) => {
-//   const projectName = req.params.project;
-//   let issues = null;
+    try {
+      const book = await Book.findByIdAndUpdate(
+        bookId,
+        { $push: { comments: comment } },
+        {
+          new: true,
+          useFindAndModify: false,
+        }
+      );
 
-//   // Filter
-//   const match = {
-//     // GET /issues?id=5f3a710fed618c093c160d11
-//     ...(req.query.id && { _id: req.query.id }),
-//     // GET /issues?author=kelvin123
-//     ...(req.query.author && { created_by: req.query.author }),
-//     // GET /issues?assigned=mathew159
-//     ...(req.query.assigned && { assigned_to: req.query.assigned }),
-//     // GET /issues?status=help%please
-//     ...(req.query.status && { status_text: req.query.status }),
-//     // GET /issues?open=false
-//     ...(req.query.open && { open: req.query.open === 'true' }),
-//   };
+      if (!book) res.status(404).send('no book exists');
 
-//   // Sorter
-//   const sort = {};
+      res
+        .status(201)
+        .json({ _id: book._id, title: book.title, comments: book.comments });
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  })
 
-//   // GET /issues?sort=created_on:desc || GET /issues?sort=updated_on:desc
-//   if (req.query.sort) {
-//     const parts = req.query.sort.split(':');
-//     sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
-//   }
+  // ======================
+  // READ BOOK
+  // ======================
+  .get(async (req, res) => {
+    try {
+      const book = await Book.findById(req.params._id);
 
-//   try {
-//     // Search project in the database and populate all issues
-//     const foundProject = await Project.findOne({
-//       name: projectName,
-//     })
-//       .populate({
-//         path: 'issues',
-//         match,
-//         options: {
-//           limit: req.query.limit,
-//           sort,
-//         },
-//       })
-//       .lean();
+      if (!book) res.status(404).send('no book exists');
 
-//     if (foundProject) {
-//       // If a project is found save issues in a variable
-//       issues = foundProject.issues;
+      res
+        .status(200)
+        .json({ _id: book._id, title: book.title, comments: book.comments });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  })
 
-//       // Loop through issues to set relative time
-//       issues.forEach((e) => {
-//         e.created_on = moment(e.created_on).fromNow();
-//         e.updated_on = e.updated_on
-//           ? moment(e.updated_on).fromNow()
-//           : e.updated_on;
-//         e.closed_on = e.closed_on
-//           ? moment(e.closed_on).fromNow()
-//           : e.closed_on;
-//       });
-//     }
+  // ======================
+  // DESTROY BOOK
+  // ======================
+  .delete(async (req, res) => {
+    try {
+      const book = await Book.findByIdAndDelete(req.params._id);
 
-//     res.render('issues', {
-//       projectName,
-//       issues,
-//     });
-//   } catch (err) {
-//     res.status(400);
-//     throw err;
-//   }
-// });
+      if (!book) res.status(404).send('no book exists');
 
-// ======================
-// UPDATE
-// ======================
-// router.get('/edit', async (req, res) => {
-//   const projectName = req.params.project;
-//   const { issueId } = req.query;
-
-//   try {
-//     // Find issue by id
-//     const issue = await Issue.findById(issueId);
-//     res.render('edit', { projectName, issue });
-//   } catch (err) {
-//     res.status(400);
-//     throw err;
-//   }
-// });
-
-// router.post('/edit', async (req, res) => {
-//   const projectName = req.params.project;
-//   const { issueId } = req.query;
-//   const { issue } = req.body;
-
-//   // Add update date
-//   issue.updated_on = Date.now();
-
-//   // Handle open/close issue
-//   if (issue.open === 'open') {
-//     issue.open = true;
-//   } else {
-//     issue.open = false;
-//     issue.closed_on = Date.now();
-//   }
-
-//   try {
-//     // Find issue by id and update
-//     await Issue.findByIdAndUpdate(issueId, issue, { useFindAndModify: false });
-//     res.redirect(`/${projectName}/issues`);
-//   } catch (err) {
-//     res.status(400);
-//     throw err;
-//   }
-// });
-
-// ======================
-// DESTROY
-// ======================
-// router.get('/delete', async (req, res) => {
-//   const projectName = req.params.project;
-//   const { issueId } = req.query;
-
-//   try {
-//     // Find issue by id and remove
-//     const issue = await Issue.findByIdAndRemove(issueId, {
-//       useFindAndModify: false,
-//     });
-//     if (!issue) return res.status(404).send;
-//     return res.redirect(`/${projectName}/issues`);
-//   } catch (err) {
-//     res.status(500);
-//     throw err;
-//   }
-// });
+      res.status(200).send('delete successful');
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
 
 module.exports = router;
